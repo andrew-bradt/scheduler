9977d36b33dce89b006f243d196b7926e62e20f4
Andrew Bradt <andrewjbradt@gmail.com>
2022-03-21 16:23:24 -0400

    apply styles based on props

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,8 +1,15 @@
 import React from "react";
+import classNames from 'classnames';
+import 'components/DayListItem.scss';
 
 export default function DayListItem(props) {
+  const dayClass = classNames('day-list__item', {
+    'day-list__item--full': !props.spots,
+    'day-list__item--selected': props.selected
+  });
+
   return (
-    <li onClick = {() => props.setDay(props.name)}>
+    <li className = {dayClass} onClick = {() => props.setDay(props.name)}>
       <h2 className='text--regular'>{props.name}</h2>
       <h3 className='text--light'>{props.spots} Spots Remaining</h3>
     </li>
