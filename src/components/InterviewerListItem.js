f5375796a70cdd12f20ef7a07b40f2d39743b8ed
Andrew Bradt <andrewjbradt@gmail.com>
2022-03-22 09:54:52 -0400

    Pass props to elements, setup handleClick function

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import 'components/InterviewerListItem.scss';
+import classNames from 'classnames';
 
-export default function InterviewerListItem ({props}) {
+export default function InterviewerListItem (props) {
+  const {id, name, avatar, selected, setInterviewer} = props;
+  const handleClick = (id) => setInterviewer(id);
   return (
-    <li className="interviewers__item">
+    <li 
+      className="interviewers__item"
+      key = {id}
+      onClick = {() => handleClick(id)}
+    >
       <img
         className="interviewers__item-image"
-        src="https://i.imgur.com/LpaY82x.png"
-        alt="Sylvia Palmer"
+        src={avatar}
+        alt={name}
       />
-      Sylvia Palmer
+      {name}
     </li>
   );
 }
