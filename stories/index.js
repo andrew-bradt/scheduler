04a4bcf91706c3958694e41e3b86ec00572b8ff0
Andrew Bradt <andrewjbradt@gmail.com>
2022-03-22 14:36:09 -0400

    use interviewer.id as argument to setInterviewer in InterviewerListItem story

--- stories/index.js
+++ stories/index.js
@@ -81,14 +81,12 @@ storiesOf("InterviewerListItem", module)
   })
   .add("Unselected", () => (
     <InterviewerListItem
-      id={interviewer.id}
       name={interviewer.name}
       avatar={interviewer.avatar}
     />
   ))
   .add("Selected", () => (
     <InterviewerListItem
-      id={interviewer.id}
       name={interviewer.name}
       avatar={interviewer.avatar}
       selected
@@ -96,10 +94,9 @@ storiesOf("InterviewerListItem", module)
   ))
   .add("Clickable", () => (
     <InterviewerListItem
-      id={interviewer.id}
       name={interviewer.name}
       avatar={interviewer.avatar}
-      setInterviewer={action("setInterviewer")}
+      setInterviewer={() => action("setInterviewer")(interviewer.id)}
     />
   ));
 
