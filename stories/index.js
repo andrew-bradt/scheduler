004e439cfdacc94740d58bafe312c4c19855a696
Andrew Bradt <andrewjbradt@gmail.com>
2022-03-22 17:42:41 -0400

    Create Status component, add to Storybook

--- stories/index.js
+++ stories/index.js
@@ -18,6 +18,7 @@ import Header from 'components/Appointment/Header';
 import Empty from 'components/Appointment/Empty';
 import Show from 'components/Appointment/Show';
 import Confirm from 'components/Appointment/Confirm';
+import Status from 'components/Appointment/Status';
 
 storiesOf("Button", module)
   .addParameters({
@@ -148,4 +149,5 @@ storiesOf('Appointment', module)
   .add('Empty', () => <Empty onAdd = {action('onAdd')}/>)
   .add('Show', () => <Show onEdit = {action('onEdit')} onDelete = {action('onDelete')}/>)
   .add('Confirm', () => <Confirm message='Are you sure?' onCancel={action('onCancel')} onConfirm={action('onConfirm')} />)
+  .add('Status', () => <Status message='Fetching Data'/>)
 ;
