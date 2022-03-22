dddbaea91f1e6dc9401ed29c98129ae4d7534186
Andrew Bradt <andrewjbradt@gmail.com>
2022-03-22 10:10:54 -0400

    add stylesheet

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import 'components/InterviewerList.scss';
 
 export default function InterviewerList (props) {
   const {interviewers, setInterviewer, interviewer} = props;
