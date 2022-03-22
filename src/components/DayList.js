27df086c41022013d413e7cf558d97d618d8b0fc
Andrew Bradt <andrewjbradt@gmail.com>
2022-03-22 14:43:52 -0400

    destructure days from props, create parsedDays variable in DayList

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -2,21 +2,18 @@ import React from 'react';
 import DayListItem from './DayListItem';
 
 export default function DayList (props) {
-  return (
-    <ul>
-      {
-        props.days.map(day => {
-          return (
-            <DayListItem
-              key={day.id}
-              name={day.name}
-              spots={day.spots}
-              selected={day.name === props.day}
-              setDay={props.setDay}
-            />
-          );
-        })
-      }
-    </ul>
-  );
+  const {days} = props;
+  const parsedDays = days.map(day => {
+    return (
+      <DayListItem
+        key={day.id}
+        name={day.name}
+        spots={day.spots}
+        selected={day.name === props.day}
+        setDay={props.setDay}
+      />
+    );
+  });
+
+  return parsedDays;
 }
