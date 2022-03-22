7bea5346f554d36de5bad21c46290c8b241a295b
Andrew Bradt <andrewjbradt@gmail.com>
2022-03-22 15:48:42 -0400

    add Appointment component and styles

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import 'components/Application/styles.scss';
+
+export default function Appointment() {
+  return (
+    <article className='appointment'></article>
+  );
+}
