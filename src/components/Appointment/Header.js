72602daecf77a0aef94b8099d5521d1b82b734fe
Andrew Bradt <andrewjbradt@gmail.com>
2022-03-23 10:33:44 -0400

    correct spelling on className for hr in Header

--- src/components/Appointment/Header.js
+++ src/components/Appointment/Header.js
@@ -6,7 +6,7 @@ export default function Header (props) {
   return (
     <header className="appointment__time">
       <h4 className="text--semi-bold">{time}</h4>
-      <hr className="appointment__seperator"/>
+      <hr className="appointment__separator"/>
     </header>
   );
 }
