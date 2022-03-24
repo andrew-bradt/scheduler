cac2d81d563494a597f3fe0027eed9a417c8cfc5
Andrew Bradt <andrewjbradt@gmail.com>
2022-03-23 20:44:41 -0400

    resolve dependency warning for useEffect

--- src/components/Application.js
+++ src/components/Application.js
@@ -52,9 +52,9 @@ export default function Application(props) {
     appointments: {}
   });
 
-  const setDay = (day) => setState({...state, day});
-  const setDays = (days) => setState({...state, days});
-  
+  const setDay = (day) => setState((prev) => ({...prev, day}));
+  const setDays = (days) => setState((prev) => ({...prev, days}));
+
   useEffect(()=>{
     axios.get('/api/days').then(res => setDays(res.data));
   }, []);
