9fe4f3aacd3bddd76e9e87aa1bb91f607155fa7d
Andrew Bradt <andrewjbradt@gmail.com>
2022-03-24 09:34:30 -0400

    make api calls to days and appointments, update state

--- src/components/Application.js
+++ src/components/Application.js
@@ -19,7 +19,12 @@ export default function Application(props) {
   const setDays = (days) => setState((prev) => ({...prev, days}));
 
   useEffect(()=>{
-    axios.get('/api/days').then(res => setDays(res.data));
+    Promise.all([
+      axios.get('/api/days'),
+      axios.get('/api/appointments')
+    ]).then(all => {
+      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data}));
+    })
   }, []);
   
   const parsedAppointments = dailyAppointments.map(appointment => <Appointment key={appointment.id} {...appointment}/>);
