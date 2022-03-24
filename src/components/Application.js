c4424b74ee11be75583cfd46325ca7ad07d22a68
Andrew Bradt <andrewjbradt@gmail.com>
2022-03-23 20:50:55 -0400

    rename appointments to dailyAppointments

--- src/components/Application.js
+++ src/components/Application.js
@@ -6,51 +6,14 @@ import Appointment from './Appointment';
 
 import "components/Application.scss";
 
-const appointments = {
-  "1": {
-    id: 1,
-    time: "12pm",
-  },
-  "2": {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer:{
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  "3": {
-    id: 3,
-    time: "2pm",
-  },
-  "4": {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer:{
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
-  },
-  "5": {
-    id: 5,
-    time: "4pm",
-  }
-};
-
 export default function Application(props) {
   const [state, setState] = useState({
     day: 'Monday',
     days: [],
     appointments: {}
   });
+
+  const dailyAppointments = [];
 
   const setDay = (day) => setState((prev) => ({...prev, day}));
   const setDays = (days) => setState((prev) => ({...prev, days}));
@@ -59,7 +22,7 @@ export default function Application(props) {
     axios.get('/api/days').then(res => setDays(res.data));
   }, []);
   
-  const parsedAppointments = Object.values(appointments).map(appointment => <Appointment key={appointment.id} {...appointment}/>);
+  const parsedAppointments = Object.values(dailyAppointments).map(appointment => <Appointment key={appointment.id} {...appointment}/>);
   
   return (
     <main className="layout">
