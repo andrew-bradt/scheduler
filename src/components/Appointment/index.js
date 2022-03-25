0bf8db3e9cba426a9a34cbddc3f00002df12ee8c
Andrew Bradt <andrewjbradt@gmail.com>
2022-03-25 13:04:10 -0400

    change mode to CREATE when clicking add button

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,11 +4,13 @@ import 'components/Appointment/styles.scss';
 import Header from './Header';
 import Show from './Show';
 import Empty from './Empty';
+import Form from './Form';
 
 import useVisualMode from '../../hooks/useVisualMode';
 
 const EMPTY = 'EMPTY';
 const SHOW = 'SHOW';
+const CREATE = 'CREATE';
 
 export default function Appointment(props) {
   const {time, id, interview} = props;
@@ -18,13 +20,14 @@ export default function Appointment(props) {
   return (
     <article className='appointment'>
       <Header time = {time}/>
-      {mode === EMPTY && <Empty/> }
+      {mode === EMPTY && <Empty onAdd = {() => transition(CREATE)}/> }
       {mode === SHOW && (
         <Show 
           student = {interview.student} 
           interviewer = {interview.interviewer} 
         />
       )}
+      {mode === CREATE && <Form interviewers = {[]} />}
     </article>
   );
 }
