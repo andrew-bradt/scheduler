5e082c115f2f0e903ab0aa77384803c6b3b2a9d3
Andrew Bradt <andrewjbradt@gmail.com>
2022-03-28 09:24:45 -0400

    create save function, pass to onSave of Form

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -13,9 +13,16 @@ const SHOW = 'SHOW';
 const CREATE = 'CREATE';
 
 export default function Appointment(props) {
-  const {time, id, interview, interviewers} = props;
+  const {time, id, interview, interviewers, bookInterview} = props;
   const {mode, transition, back} = useVisualMode((interview) ? SHOW : EMPTY);
   
+  const save = (name, interviewer) => {
+    const interview = {
+      student: name,
+      interviewer
+    };
+    bookInterview(id, interview);
+  };
 
   return (
     <article className='appointment'>
@@ -27,7 +34,7 @@ export default function Appointment(props) {
           interviewer = {interview.interviewer} 
         />
       )}
-      {mode === CREATE && <Form interviewers = {interviewers} onCancel = {back}/>}
+      {mode === CREATE && <Form interviewers = {interviewers} onCancel = {back} onSave={save}/>}
     </article>
   );
 }
