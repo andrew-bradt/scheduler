9be1152da69b5c707a9ed45b8d5e2228cb216527
Andrew Bradt <andrewjbradt@gmail.com>
2022-03-28 09:38:13 -0400

    Update state with new appointment with bookInterview

--- src/components/Application.js
+++ src/components/Application.js
@@ -19,7 +19,17 @@ export default function Application(props) {
   const setDays = (days) => setState((prev) => ({...prev, days}));
 
   const bookInterview = (id, interview) => {
-    console.log(id, interview);
+    const appointment = {
+      ...state.appointments[id],
+      interview: {...interview}
+    };
+
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+
+    setState({...state, appointments});
   };
 
   useEffect(()=>{
