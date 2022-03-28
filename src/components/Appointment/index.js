6738e917095a1290466dca8e7f4976d5d1b11760
Andrew Bradt <andrewjbradt@gmail.com>
2022-03-28 16:13:35 -0400

    Catch error when saving or deleting an appointment, render Error component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -7,6 +7,7 @@ import Empty from './Empty';
 import Form from './Form';
 import Status from './Status';
 import Confirm from './Confirm';
+import Error from './Error';
 
 import useVisualMode from '../../hooks/useVisualMode';
 
@@ -17,6 +18,8 @@ const SAVING = 'SAVING';
 const CONFIRM = 'CONFIRM';
 const DELETING = 'DELETING';
 const EDIT = 'EDIT';
+const ERROR_SAVE = 'ERROR_SAVE';
+const ERROR_DELETE = 'ERROR_DELETE';
 
 export default function Appointment(props) {
   const {time, id, interview, interviewers, bookInterview, cancelInterview} = props;
@@ -29,7 +32,8 @@ export default function Appointment(props) {
     };
     transition(SAVING);
     bookInterview(id, interview)
-      .then(()=> transition(SHOW));
+      .then(()=> transition(SHOW))
+      .catch(()=>transition(ERROR_SAVE));
   };
 
   const onEdit = () => transition(EDIT);
@@ -38,7 +42,8 @@ export default function Appointment(props) {
   const onConfirmDelete = () => {
     transition(DELETING);
     cancelInterview(id)
-      .then(()=>transition(EMPTY));
+      .then(()=>transition(EMPTY))
+      .catch(()=>transition(ERROR_DELETE));
   };
 
   return (
@@ -65,6 +70,8 @@ export default function Appointment(props) {
       {mode === CONFIRM && <Confirm message='Are you sure you would like to delete?' onConfirm = {onConfirmDelete} onCancel = {back}/>}
       {mode === SAVING && <Status message='Saving' />}
       {mode === DELETING && <Status message='Deleting' />}
+      {mode === ERROR_SAVE && <Error message='Error Saving' />}
+      {mode === ERROR_DELETE && <Error message='Error Deleting' />}
     </article>
   );
 }
