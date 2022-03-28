17a8f9b3846b7e147f707fb38b634feb38ba4d72
Andrew Bradt <andrewjbradt@gmail.com>
2022-03-28 19:27:29 -0400

    refactor useVisualMode to use prev

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -3,19 +3,18 @@ import {useState} from 'react';
 export default function useVisualMode (initial) {
   const [history, setHistory] = useState([initial]);
   
+  const indexFromEnd = history.length - 1;
+
   function transition (newMode, shouldReplace) {
-    const historyCopy = [...history];
     if (shouldReplace) {
-      historyCopy.pop()
+      return setHistory(prev => prev.slice(0, indexFromEnd).push(newMode));
     }
-    setHistory([...historyCopy, newMode]);
+    setHistory(prev => [...prev, newMode]);
   }
 
   function back () {
     if (history.length > 1) {
-      const historyCopy = [...history];
-      historyCopy.pop();
-      setHistory(historyCopy);
+      setHistory(prev => prev.slice(0, indexFromEnd));
     }
   }
 
