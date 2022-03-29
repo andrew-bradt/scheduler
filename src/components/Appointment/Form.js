9c28cea031d14c3a37e4802a56ed2ed0f86b8566
Andrew Bradt <andrewjbradt@gmail.com>
2022-03-29 15:38:27 -0400

    add data-testid to input on Form component, add tests for rendering with and without student name

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -27,6 +27,7 @@ export default function Form (props) {
       <section className="appointment__card-left">
         <form autoComplete="off" onSubmit = {(e) => e.preventDefault()}>
           <input
+            data-testid = 'student-name-input'
             className="appointment__create-input text--semi-bold"
             name="name"
             type="text"
