05fb58d485d980c0b949f2f34365bbc71903f95b
Andrew Bradt <andrewjbradt@gmail.com>
2022-03-29 13:07:37 -0400

    Implememnt updateSpots

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
+import { getAppointmentsForDay } from 'helpers/selectors';
 
 export default function useApplicationData () {
   const [state, setState] = useState({
@@ -14,7 +15,8 @@ export default function useApplicationData () {
       axios.get('/api/days'),
       axios.get('/api/appointments'),
       axios.get('/api/interviewers')
-    ]).then(all => {
+    ])
+    .then(all => {
       const [days, appointments, interviewers] = all;
       setState(prev => ({
         ...prev, 
@@ -25,18 +27,36 @@ export default function useApplicationData () {
     })
   }, []);
   
-  const setDay = (day) => setState((prev) => ({...prev, day}));
-// const setDays = (days) => setState((prev) => ({...prev, days}));
+  const setDay = (day) => setState((prev) => ({...prev , day}));
 
+  const updateSpots = (state, appointments) => {
+    const stateForDay = state.days.find(day => day.name === state.day); 
+    const appointmentsForDay = stateForDay.appointments.map(id => appointments[id]);
+    const availableSpots = appointmentsForDay.filter(apt => !apt.interview).length;
+    const updatedDay = {
+      ...stateForDay,
+      spots: availableSpots
+    };
+    const days = [...state.days];
+    days[stateForDay.id - 1] = updatedDay;
+  };
+  
   const bookInterview = (id, interview) => {
     const appointment = {
-    ...state.appointments[id],
+      ...state.appointments[id],
       interview: {...interview}
     };
+
     const appointments = {
       ...state.appointments,
       [id]: appointment
     };
+    
+    updateSpots(state, appointments);
+
+    // const days = [...state.days];
+    // days[dayID] = nextDayState;
+    // console.log(days);
     return axios.put(`/api/appointments/${id}`, appointment)
       .then(()=>setState({...state, appointments}));
   };
