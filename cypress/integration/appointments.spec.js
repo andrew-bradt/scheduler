13031597ef6d3974034779f6310a391be8d2fe56
Andrew Bradt <andrewjbradt@gmail.com>
2022-03-30 10:21:57 -0400

    add beforeEach block to reset db, visit / and click Monday

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -1,15 +1,17 @@
 describe('Appointments', () => {
-  it('should book an interview', () => {
+  beforeEach(() => {
     cy.request('GET', '/api/debug/reset');
     cy.visit('/');
     cy.contains('Monday');
+  });
 
-    cy.get("img[alt='Add']")
+  it('should book an interview', () => {
+    cy.get("[alt='Add']")
       .first()
       .click();
 
     cy.get("[data-testid='student-name-input'").type('Lydia Miller-Jones');
-    cy.get("img[alt='Sylvia Palmer']").click();
+    cy.get("[alt='Sylvia Palmer']").click();
     cy.contains('Save').click();
 
     cy.contains('.appointment__card--show', 'Lydia Miller-Jones');
